f0ff4ada58680910f5739445accf228258d5282e
Reece <reecepbcups@gmail.com>
2022-05-02 20:43:53 +0200

    comment

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,3 +1,5 @@
+# May be easier to just put all config locations in here to reduce complexity of config.yml having some & here having 1.
+
 PATH_TO_CONFIG_FILE = "/root/minecraft-panel/config.yml"
 
 from utils.yaml import Yaml

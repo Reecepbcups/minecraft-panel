89048c6eb7d8946d6c57e7e5456482acc45add04
Reece <reecepbcups@gmail.com>
2022-05-04 06:01:55 +0200

    Broke up everything into smaller functions

--- src/server_creator.py
+++ src/server_creator.py
@@ -20,7 +20,7 @@ def paper_install():
     PAPER_V2_API_VERSION = "https://papermc.io/api/v2/projects/{project}/versions/{version}"
     PAPER_V2_API = "https://papermc.io/api/v2/projects/{project}/versions/{version}/builds/{build}/downloads/{download}"
 
-    project = cinput("&3Server Type [waterfall, (paper)]>> ") or "paper"
+    project = cinput("&bServer Type &f[waterfall, (paper)]&b>> ") or "paper"
     if project not in ['waterfall', 'paper']:
         if project in ["None", "exit"]:
             return
@@ -33,7 +33,7 @@ def paper_install():
     cprint("&a\nVersions you can use:\n" + versionGroupString)
 
     latest = versionGroupString.split(', ')[-1]
-    version = cinput(f"&3Server Version ({latest})>> ") or latest
+    version = cinput(f"&bServer Version &f({latest})&b>> ") or latest
     if version not in versionGroupString:
         if project in ["None", "exit"]:
             return
@@ -80,93 +80,43 @@ nameToID = { # Spigot IDS from url. Uses spiget API to get
 }
 IDtoName = {v: k for k, v in nameToID.items()}
 
-# TODO: Break this up into chunks
 class ServerCreator():
     # move inputs into console.py
-    # cprint("&3Server Installer! (Press enter to accept the default value)")
+    # cprint("&bServer Installer! (Press enter to accept the default value)")
 
     def __init__(self):
-        server_loc = CONFIG.get("SERVER_DIRECTORY")
-        SERVER_NAME = cinput("&bServer Name: ") or ("myServer-" + get_time())
+        cfiglet("&a", "Server Creator", clearScreen=True)
 
-        if not os.path.exists(server_loc):
-            os.mkdir(server_loc)
-        server_path = f'{server_loc}/{SERVER_NAME}'
-        self.server_path = server_path # so isntallplugins works
-
-        if os.path.exists(server_path):
-            cprint(f"&cThis is already a server {server_path=}")
-            exit(1) 
+        servers_dir = CONFIG.get("SERVER_DIRECTORY")
+        SERVER_NAME, server_path = self.createServerFolder(servers_dir)
+        self.server_path = server_path
 
         # Add config check where if serverloc is not found, it will ask the user for the location of servers & make config
         
-        jarName = paper_install()
-        os.mkdir(server_path)
-        # print(jarName); exit()
+        JAR_NAME = self.downloadPaper()
+        RAM = cinput("&bRam amount: &f[500M/(4G) etc...] &b>> ") or "4G"
 
-        # Download jar into server directly
-        downloadCache = str(CONFIG.get("DOWNLOAD_CACHE"))
-        file_path = f'{downloadCache}/{jarName}'
-        shutil.copyfile(file_path,f"{server_path}/{jarName}") 
+        allow_end = cinput("&bAllow end? &f[(true)/false] &b>> ") or "true" 
+        allow_nether = cinput("&bAllow Nether &f[(true)/false]&b>> ") or "true"            
+        max_players = cinput("&bMax Players &f(100) &b>> ") or "100"
+        view_distance = cinput("&bView Distance &f(8)&b>> ") or "8"
+        advancements = cinput("&bEnable Advancements &f[(true)/false]&b>> ") or "true"
 
-        # other inputs
-        RAM = cinput("&3Ram amount: [500M/(2G) etc...] >> ") or "2G"
-        JAR_NAME=f"{jarName}"
+        isBehindBungee = cinput("&bBehind Bungee? &f[(true)/false] &b>> ") or "true" 
+        port = int(cinput("&bPort: &f(25565) &b>> ") or 25565)
 
-        # Start file creation.
-        startFile = f'''#!/bin/sh
-# Reecepbcups - start.sh script for servers. EULA Auto Accepted.
-            
-MEM_HEAP="{RAM}"
-JAR_FILE="{JAR_NAME}"
-
-'''
-
-        # If RAM usage is >12GB, optomized java arguments are used
-        if 'g' in RAM.lower() and int(''.join(filter(str.isdigit, RAM))) > 12: # remove all letters from RAM, so its only the #    
-            startFile += 'JAVA_ARGS="-Dfile.encoding=utf-8 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=40 -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+AlwaysPreTouch  -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=8 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=true -Daikars.new.flags=true -Dcom.mojang.eula.agree=true"'
-        else:
-            startFile += 'JAVA_ARGS="-Dfile.encoding=utf-8 -Dcom.mojang.eula.agree=true"'
-
-        startFile += f'''
-
-while true; do
-    java -Xms$MEM_HEAP -Xmx$MEM_HEAP $JAVA_ARGS -jar $JAR_FILE nogui
-    echo "Restarting server in 5 seconds"
-    sleep 4
-    echo "Restarting..."
-    sleep 1
-done'''
-
-        # print(startFile)
-
-        # write startFile to test.sh
-        with open(server_path+'/start.sh', 'w') as file:
-            file.write(startFile)
-        print("start.sh file made")
-
-        os.system(f"chmod +x {server_path}/start.sh")
-
-        # TODO: Add proxy stuff here
+        self.createStartFile(RAM, JAR_NAME)
 
         # spigot.yml
-        with open(server_path+"/spigot.yml", "a") as file:
-            isBehindBungee = cinput("&3Behind Bungee? [(true)/false] >> ") or "true"  
-            if isBehindBungee != "true":
-                isBehindBungee = "false"          
-            file.write(f"""settings:\n  bungeecord: {isBehindBungee}\n  restart-on-crash: false""")
+        with open(server_path+"/spigot.yml", "a") as file:                   
+            file.write(f"""settings:\n  bungeecord: {isBehindBungee}\n  restart-on-crash: false\nadvancements:\n    disable-saving: {advancements}""")
 
         with open(server_path+"/bukkit.yml", "a") as bukkitFile:
-            allowEnd = cinput("&3Allow end? [(true)/false] >> ") or "true"          
-            bukkitFile.write(f"""settings:\n  allow-end: {allowEnd}""")
+            bukkitFile.write(f"""settings:\n  allow-end: {allow_end}""")
 
         # server.properties
         with open(server_path+"/server.properties", "a") as file:
             # Add check to ensure port can not be set if another one uses it
-            allow_nether = cinput("&3Allow Nether [(true)/false]>> ") or "true"            
-            max_players = cinput("&3Max Players (200) >> ") or "200"
-            view_distance = cinput("&3View Distance (8)>> ") or "8"
-            port = int(cinput("&3Port: [(25565)] >> ") or 25565)
             file.write(f"server-port={str(port)}\n")
             file.write(f"allow-nether={allow_nether}\n")
             file.write(f"max-players={max_players}\n")
@@ -177,19 +127,15 @@ done'''
                 file.write("network-compression-threshold=256\n")
             else:
                 file.write("online-mode=false\n")
-                file.write("network-compression-threshold=-1\n")                
+                file.write("network-compression-threshold=-1\n")
 
-            # use-native-transport=false if you get spam for unable to access address of buffer
-
-        # ask if we should add to config? default yes
-        # change terminal to server_path and exit
         os.chdir(server_path)
-        os.mkdir("logs")
-        os.mkdir("plugins")
+        os.mkdir("logs"); os.mkdir("plugins")
 
-        
+        # ask if we should add to global config.yml? default yes
+
         # Ask user if they want to preisntall some plugins
-        installPlugins = cinput("&3Install common plugins? [(yes)/no]> ") or 'yes'
+        installPlugins = cinput("&bInstall common plugins? [(yes)/no]> ") or 'yes'
         if installPlugins.lower().startswith('y'):
             self.installPluginsToServer()
 
@@ -201,15 +147,58 @@ done'''
             cprint(f"&aYour server is now live at: 127.0.0.1:{port} OR {getPublicIPAddress()}:{port}")
         cprint(f"&aServer created at {server_path=}")
 
-        doStart = cinput("&3Start server now? [yes/(no)] >> ") or "yes"
+        doStart = cinput("&bStart server now? [yes/(no)] &b>> ") or "yes"
         s = Server(SERVER_NAME)
         if doStart.lower().startswith('y'):
             s.start_server()
-            cprint("&aServer {SERVER_NAME} started")
 
-        connectToConsoleOfServer = cinput("&3Connect to console of server? [yes/(no)] >> ") or "no"
-        if connectToConsoleOfServer.lower().startswith('y'):
-            s.enter_console()
+            connect_console = cinput("&bConnect to console of server? [yes/(no)] &b>> ") or "no"
+            if connect_console.lower().startswith('y'):
+                s.enter_console()
+
+    def createServerFolder(self, servers_dir) -> str:
+        defaultName = "myServer-" + get_time()
+        SERVER_NAME = cinput(f"&bServer Name &f({defaultName})&b>> ") or defaultName
+        if not os.path.exists(servers_dir):
+            os.mkdir(servers_dir)
+        
+        server_path = f'{servers_dir}/{SERVER_NAME}'
+
+        if os.path.exists(server_path):
+            cprint(f"&cThis is already a server {server_path=}.\n&aUsing default name {defaultName}")
+            SERVER_NAME = defaultName
+            server_path = f'{servers_dir}/{SERVER_NAME}'
+            
+        os.mkdir(server_path)
+        return SERVER_NAME, server_path
+
+    def downloadPaper(self) -> str:
+        jarName = paper_install() # print(jarName); exit()
+
+        # Moves file from the cache folder -> the server parent folder
+        downloadCache = str(CONFIG.get("DOWNLOAD_CACHE"))
+        file_path = f'{downloadCache}/{jarName}'
+        shutil.copyfile(file_path,f"{self.server_path}/{jarName}")
+        return jarName
+
+    def createStartFile(self, RAM, JAR_NAME):
+        # Start file creation.
+        startFile = f'''#!/bin/sh\n# Reecepbcups - start.sh script for servers. EULA Auto Accepted.\n\nMEM_HEAP="{RAM}"\nJAR_FILE="{JAR_NAME}"\n'''
+        # If RAM usage is >12GB, optomized java arguments are used
+        if 'g' in RAM.lower() and int(''.join(filter(str.isdigit, RAM))) > 12: # remove all letters from RAM, so its only the #    
+            startFile += 'JAVA_ARGS="-Dfile.encoding=utf-8 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=40 -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:-OmitStackTraceInFastThrow -XX:+AlwaysPreTouch  -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=8 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=true -Daikars.new.flags=true -Dcom.mojang.eula.agree=true"'
+        else:
+            startFile += 'JAVA_ARGS="-Dfile.encoding=utf-8 -Dcom.mojang.eula.agree=true"'
+
+        startFile += f'''\n\nwhile true; do\n\tjava -Xms$MEM_HEAP -Xmx$MEM_HEAP $JAVA_ARGS -jar $JAR_FILE nogui\n\techo "Restarting server in 5 seconds"\n\tsleep 4\n\techo "Restarting..."\n\tsleep 1\ndone'''
+
+        # write startFile to test.sh
+        with open(self.server_path+'/start.sh', 'w') as file:
+            file.write(startFile)
+            # print("start.sh file made")
+        os.system(f"chmod +x {self.server_path}/start.sh")
+        
+
 
 
     def installPluginsToServer(self):
@@ -225,7 +214,7 @@ done'''
 
 
 # import requests
-def downloadResourceFromSpigot(resourceID, folderPath=os.getcwd()):
+def downloadResourceFromSpigot(resourceID, folderPath=os.getcwd(), debug=False):
     url = f"https://api.spiget.org/v2/resources/{resourceID}/download"
 
     response = requests.get(url)
@@ -236,7 +225,7 @@ def downloadResourceFromSpigot(resourceID, folderPath=os.getcwd()):
     with open(jarName, "wb") as jar:
         jar.write(response.content)
 
-    print(f"Downloaded {pluginName} to {jarName}")
+    if debug: print(f"Downloaded {pluginName} to {jarName}")
 
 if __name__ == "__main__":
     # TEst dwnload

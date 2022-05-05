69b2b4f2e265ffb65bbab1cc8a7a384892bbc0ae
Reece <reecepbcups@gmail.com>
2022-05-05 18:43:25 -0500

    change of path for config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,6 @@
 # May be easier to just put all config locations in here to reduce complexity of config.yml having some & here having 1.
 
-PATH_TO_CONFIG_FILE = "/root/minecraft-panel/config.yml"
+PATH_TO_CONFIG_FILE = "/home/reece/Desktop/minecraft-panel/config.yml"
 
 from utils.yaml import Yaml
 CONFIG = Yaml(PATH_TO_CONFIG_FILE).loadConfig()

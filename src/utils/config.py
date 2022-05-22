8c7619ddd75ee8840c4b2a86261fce869681cdc5
Reece Williams <Reecepbcups@gmail.com>
2022-05-22 22:05:06 +0200

    Auto get default config if incorrect one is provided

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,15 @@
 # May be easier to just put all config locations in here to reduce complexity of config.yml having some & here having 1.
+import os
 
 PATH_TO_CONFIG_FILE = "/home/reece/Desktop/minecraft-panel/config.yml"
+
+# check if PATH_TO_CONFIG_FILE is real. If not, get current file location & get the parent directories (like: cd ../../)
+if not os.path.isfile(PATH_TO_CONFIG_FILE):
+    # print(f"Config file not found {PATH_TO_CONFIG_FILE}. Getting default config location")    
+    path = os.path.realpath(__file__) # get current directory    
+    path = os.path.dirname(path) # get parent directory (src)
+    PATH_TO_CONFIG_FILE = os.path.dirname(os.path.dirname(path)) + "/config.yml" # get parent directory (minecraft_panel/config.yml)
+    print(PATH_TO_CONFIG_FILE)
 
 from utils.yaml import Yaml
 CONFIG = Yaml(PATH_TO_CONFIG_FILE).loadConfig()
